d9d3817eb3f6eb7dfffb3faecaace0f851d15f7e
Niv Saar <nivordsar@gmail.com>
2019-02-14 12:34:51 +0200

    added txt input events

--- 1 Thursday - 2100/js/meme-controller.js
+++ 1 Thursday - 2100/js/meme-controller.js
@@ -2,29 +2,35 @@
 
 function init() {
     initMeme();
-    render();
+    renderMeme();
 }
 
-function render() {
+function renderMeme() {
     var meme = readMeme();
     console.table(meme);
 }
 
-function onChangeProp() {
-    updateMeme('imgId', 2);
-    render();
+function onChangeMemeProp(id, value) {
+    updateMemeProp(id, value);
+    renderMeme();
 }
 
+function onChangeTextProp(id, value) {
+    updateTextProp(id, value);
+    renderMeme();
+}
 
-/* CONTROLLER*/
-// init()
-// render()
-// onEvent()
-
-/* MODEL */
-// init()
-// create()
-// add()
-// remove()
-// update()
-// read() => get for display
+function onSubmitForm(ev) {
+    event.preventDefault()
+    let meme = {
+        imgId: readMeme().imgId,
+        txts: {
+            content: document.querySelector('#memeForm #content').value,
+            size: document.querySelector('#memeForm #size').value,
+            align: document.querySelector('#memeForm #align').value,
+            color: document.querySelector('#memeForm #color').value
+        }
+    }
+    genMeme(meme);
+    renderMeme();
+}
